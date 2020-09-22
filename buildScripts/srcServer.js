924702a162b8fb341723f0e7f78b997809462859
Utuedor Binah <doctorbinah@yahoo.com>
2020-09-22 22:12:16 +0100

    added index.css, index.js and webpack.config.dev.js files. also modified srcServer.js, .babelrc files and added new dependencies to package.json

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,13 +1,23 @@
 import express from "express";
 import path from "path";
 import open from "open";
+import webpack from "webpack";
+import WebpackDevMiddleWare from "webpack-dev-middleware";
+import config from "../webpack.config.dev.js";
 
 const __dirname = path.resolve();
 const port = 3000;
 const app = express();
+const compiler = webpack(config);
+
+app.use(
+  WebpackDevMiddleWare(compiler, {
+    publicPath: config.output.publicPath,
+  })
+);
 
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "../js-dev-env-demo/src/index.html"));
+  res.sendFile(path.join(__dirname, "../js--demodev-env/src/index.html"));
 });
 
 app.listen(port, function (err) {
---
+++ webpack.config.dev.js
@@ -0,0 +1,52 @@
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import path from "path";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+export default {
+  mode: "development",
+  devtool: "inline-source-map",
+  target: "web",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/",
+    filename: "bundle.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+            },
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: true },
+          },
+        ],
+      },
+    ],
+  },
+};
